c7ec5782d8a77b7da2774f9e967296e5c082e560
prakash <pcchandra1989@gmail.com>
2018-12-01 14:15:39 +0000

    add new

---
+++ Apache/recipes/config.rb
@@ -0,0 +1,10 @@
+#
+## Cookbook:: httpd
+## Recipe:: default
+##
+## Copyright:: 2018, The Authors, All Rights Reserved.
+
+cookbook_file "/var/www/html/index.html" do
+  source "index.html.erb"
+  mode "0644"
+end
